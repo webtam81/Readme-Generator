67978806420b01097ca86956712c84c845bcd2aa
webtam81 <t.h@tammyharris.co.uk>
2023-10-07 02:03:01 +0100

    question array

--- index.js
+++ index.js
@@ -4,8 +4,48 @@ const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
+//project title, description, table of contents, installtion, usage, license, contributing, tests, questions
 const questions = [
-
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Please enter your project title: '
+    },
+    {
+        type: 'input',
+        name: 'desc',
+        message: 'Please enter your project description: '
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Please enter your installation instructions: '
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Please enter usage information: '
+    },
+    {
+        type: 'input',
+        name: 'contribute',
+        message: 'Please enter contribution guidelines: '
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Please enter test infomation: ' //check wording/what this is for
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your GitHub username?'
+    }
 ];
 
 // function to write README file
@@ -14,6 +54,8 @@ function writeToFile(fileName, data) {
 
 // function to initialize program
 function init() {
+//rename old readme?
+//run questions
 
 }
 
